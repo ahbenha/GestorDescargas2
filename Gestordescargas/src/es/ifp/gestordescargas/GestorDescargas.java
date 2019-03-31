acf7def64113c4ac872c135237cdf46d98b034ff
ahmedbenhamou <ahmedbenhamou@192.168.0.113>
2019-03-31 18:05:55 +0200

    Primer commit

---
+++ Gestordescargas/src/es/ifp/gestordescargas/GestorDescargas.java
@@ -0,0 +1,16 @@
+
+package es.ifp.gestordescargas;
+
+/**
+ *
+ * @author Santiago Faci
+ */
+public class GestorDescargas 
+{
+
+    public static void main(String[] args) {
+        Ventana ventana = new Ventana();
+        ventana.setVisible(true);
+    }
+    
+}
